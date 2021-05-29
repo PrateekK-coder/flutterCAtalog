546ce1c5082dc5163fe39dbc46ddbca0612db50b
Prateek Kumar <prateeksharma3712@gmail.com>
2021-05-29 11:29:32 +0530

    json file added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/items.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/models/items.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  void initState() {}
+
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
+    final dummyList = List.generate(9, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -11,17 +22,23 @@ class Homepage extends StatelessWidget {
             padding: const EdgeInsets.only(right: 40.0),
             child: Text(
               "Catalog App",
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(color: Colors.black),
             ),
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of code"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidgget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
